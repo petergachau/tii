65413a14a7fa462a0e93692c70011517cd9e8509
petergachau <83207831+petergachau@users.noreply.github.com>
2023-01-08 13:35:56 +0300

    hello

--- index.js
+++ index.js
@@ -16,18 +16,25 @@ import friendsRouter from './routes/userFriends.js'
 
 
 // Configureations
-
+// headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+// headers.append('Access-Control-Allow-Credentials', 'true');
 const __filename=fileURLToPath(import.meta.url);
 const __dirname=path.dirname(__filename);
 const app = express();
 dotenv.config();
 
-app.use(morgan("dev"));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy:'cross-origin'}));
+app.use(morgan('common'))
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:false}))
+
+
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
-app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({policy:'cross-origin'}))
+
+
 app.use('assets', express.static(path.join(__dirname,'public/assets')))
 app.use("/users", userRouter); // http://localhost:5000/users/signup
 app.use("/tour", tourRouter);
