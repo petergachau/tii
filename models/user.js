6cf9abf8683b5ae4f4cbaf914b64ff9407dcce10
petergachau <83207831+petergachau@users.noreply.github.com>
2023-01-08 13:23:31 +0300

    jh

--- models/user.js
+++ models/user.js
@@ -4,10 +4,10 @@ const userSchema = mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: false },
-  googleId: { type: String, required: false },
+  googleId: { type: String},
   admin:{type:String,default:false},
   imageFile:{ type: String},
-  userName:{ type: String},
+  // userName:{ type: String},
   id: { type: String },
   friends:{
     type:Array,
